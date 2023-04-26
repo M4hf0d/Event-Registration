897dafde0360872bc7e07cd8156aad22afe6a6e9
Sayah Mahfoud Abdel Ali <73748467+M4hf0d@users.noreply.github.com>
2023-04-26 23:01:18 +0200

    origins allowed

--- codewars/settings.py
+++ codewars/settings.py
@@ -58,6 +58,43 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3001",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000"
+]
+
+CORS_ORIGIN_WHITELIST = [
+     "http://localhost:3001",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000"
+]
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ORIGIN_ALLOW_ALL = True
+
+CORS_ALLOW_CREDENTIALS= True
+CORS_ALLOW_METHODS = [
+'DELETE',
+'GET',
+'OPTIONS',
+'PATCH',
+'POST',
+'PUT',
+]
+
+CORS_ALLOW_HEADERS = [
+'accept',
+'accept-encoding',
+'authorization',
+'content-type',
+'dnt',
+'origin',
+'user-agent',
+'x-csrftoken',
+'x-requested-with',
+]
+
 ROOT_URLCONF = "codewars.urls"
 
 TEMPLATES = [
